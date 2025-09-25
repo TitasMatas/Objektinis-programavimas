1df6eb282b9b7d2772ceed51ee48c6a9ea45dce5
TitasMatas <tlukonas@gmail.com>
2025-09-26 01:13:23 +0300

    Expanded to execute in functions

--- v.pradine.cpp
+++ v.pradine.cpp
@@ -6,17 +6,49 @@
 
 using namespace std;
 
-int main() {
-    int Balbai, KiekisBalu, KiekisStudentu;
-    int n;
+double mediana(const vector<int>& Balai);
+void ivedimas(vector<string>& vardas, vector<string>& pavarde,vector<vector<int>>& NamuDarbuBalai,vector<double>& Vidurkis, vector<double>& Mediana,int KiekisStudentu);
 
+int main() {
+    int KiekisStudentu;
     cout << "Kiek studentų: ";
     cin >> KiekisStudentu;
+
     vector<string> vardas(KiekisStudentu);
     vector<string> pavarde(KiekisStudentu);
     vector<vector<int>> NamuDarbuBalai(KiekisStudentu);
     vector<double> Vidurkis(KiekisStudentu);
     vector<double> Mediana(KiekisStudentu);
+
+    ivedimas(vardas, pavarde, NamuDarbuBalai, Vidurkis, Mediana, KiekisStudentu);
+
+    cout << "\nVardas       Pavardė       Vidurkis   Mediana\n";
+    cout << "----------------------------------------------\n";
+
+
+    for (int j = 0; j < KiekisStudentu; ++j) {
+        cout << setw(12) << left << vardas[j] << setw(14) << left << pavarde[j] << fixed << setprecision(2) << setw(10) << right << Vidurkis[j] << setw(10) << right << Mediana[j] << endl;
+    }
+
+    return 0;
+}
+
+double mediana(const vector<int>& Balai) {
+    int n = Balai.size();
+    if (n == 0) return 0.0;
+
+    if (n % 2 == 0) {
+        return (Balai[n / 2 - 1] + Balai[n / 2]) / 2.0;
+    } else {
+        return Balai[n / 2];
+    }
+}
+
+
+
+void ivedimas(vector<string>& vardas, vector<string>& pavarde,vector<vector<int>>& NamuDarbuBalai,vector<double>& Vidurkis, vector<double>& Mediana,int KiekisStudentu)
+{
+    int Balbai, KiekisBalu;
 
     for(int i = 0; i < KiekisStudentu; ++i)
     {
@@ -34,26 +66,8 @@ int main() {
             Vidurkis[i] += Balbai;
         }
         sort(NamuDarbuBalai[i].begin(), NamuDarbuBalai[i].end());
-
-        n = NamuDarbuBalai[i].size();
-        if (n % 2 == 0) {
-            Mediana[i] = (NamuDarbuBalai[i][n/2 - 1] + NamuDarbuBalai[i][n/2]) / 2.0;
-        } else {
-            Mediana[i] = NamuDarbuBalai[i][n/2];
-        }
-
+        Mediana[i] = mediana(NamuDarbuBalai[i]);
 
         Vidurkis[i] /= KiekisBalu;
     }
-
-cout << "\nVardas       Pavardė       Vidurkis   Mediana\n";
-cout << "----------------------------------------------\n";
-
-
-for (int j = 0; j < KiekisBalu; ++j) {
-    cout << setw(12) << left << vardas[j] << setw(14) << left << pavarde[j] << fixed << setprecision(2) << setw(10) << right << Vidurkis[j] << setw(10) << right << Mediana[j] << endl;
 }
-
-    return 0;
-}
-
